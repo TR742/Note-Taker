2e94c866f21d5d8b061a12e7565d46a951261b84
TR742 <Tyrich1442@gmail.com>
2022-04-25 20:49:42 -0700

    notes html loads

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./routes/api.js');
+// const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -18,14 +18,9 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
+// GET Route for notes page
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-// Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
 );
 
 app.listen(PORT, () =>
