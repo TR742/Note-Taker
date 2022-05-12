220d08111176f68b76d54e73c47511f3694e8ec1
TR742 <Tyrich1442@gmail.com>
2022-05-11 21:41:18 -0700

    Updated server.js and index.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./routes/index.js');
+const api = require('./routes/index.js');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -18,9 +18,9 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for notes page
+// GET Route for feedback page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
 app.listen(PORT, () =>
